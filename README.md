a766dc036530388be95d944493b11a0f3917ab9d
John Flesch <john@fles.ch>
2018-11-08 15:17:03 -0600

    Upgrades to Now v2

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 Want to enjoy an engineering blog from the list on [kilimchoi/engineering-blogs](https://github.com/kilimchoi/engineering-blogs/) but can't decide?
 
-
-#### :wave: <https://random-engineering-blog-mfqxcuddph.now.sh/>
+#### :wave: <https://random-engineering-blog.now.sh/>
--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-'use strict';
-
-const { send } = require('micro');
 const { get } = require('got');
 
 module.exports = async (req, res) => {
-
   // Grab the README.md from https://github.com/kilimchoi/engineering-blogs
-  const { body = '' } = await get('https://raw.githubusercontent.com/kilimchoi/engineering-blogs/master/README.md');
+  const { body = '' } = await get(
+    'https://raw.githubusercontent.com/kilimchoi/engineering-blogs/master/README.md',
+  );
 
   // Extract just the blog links. It's not perfect, but it'll do.
-  const blogs = body.match(/http(.*)\/$/mg) || ['https://github.com/kilimchoi/engineering-blogs'];
+  const blogs = body.match(/http(.*)\/$/gm) || [
+    'https://github.com/kilimchoi/engineering-blogs',
+  ];
 
   // Select a random blog from the list.
   const blog = blogs[Math.floor(Math.random() * blogs.length - 1)];
 
   // Go to there.
-  res.setHeader('Location', blog);
-  send(res, 307, blog);
-  
+  res.writeHead(307, { Location: blog });
+  res.end(`Redirecting to <a href="${blog}">${blog}</a>...`);
 };
